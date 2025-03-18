2aef8ade676cb50a9e61992ca0c34ff96b6ea4d2
KaiSong06 <kai.song421@gmail.com>
2025-03-18 18:07:50 -0400

    Created register component

---
+++ src/Components/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Register() {
+    return (
+        <form action="">
+            <input type="email" placeholder="email"/><br/>
+            <input type="password" placeholder="password"/><br/>
+            <button type="submit">Register</button>
+        </form>
+    )
+}
+
+export default Register;
